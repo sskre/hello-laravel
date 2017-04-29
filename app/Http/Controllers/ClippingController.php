76ff4418a6f2baa6bcd89baab98b6ca5c4b231fe
sskre <sskre@users.noreply.github.com>
2017-04-30 01:06:12 +0700

    artisan make:controller --model=Clipping ClippingController

---
+++ app/Http/Controllers/ClippingController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Clipping;
+use Illuminate\Http\Request;
+
+class ClippingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Clipping  $clipping
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Clipping $clipping)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Clipping  $clipping
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Clipping $clipping)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Clipping  $clipping
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Clipping $clipping)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Clipping  $clipping
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Clipping $clipping)
+    {
+        //
+    }
+}
